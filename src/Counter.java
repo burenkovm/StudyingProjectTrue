43c2dfafb79289a0acaf3d6ca1d249627e491a24
mikki <burenkovmikhail95@gmail.com>
2023-05-28 17:47:13 +0400

    Добавлена реализация класса Counter. Два конструктора: один с полем name, другой с полем counter. Добавлено три метода increment,decrement, info. В main прошло тестирование нового класса.

--- src/Counter.java
+++ src/Counter.java
@@ -1,5 +1,26 @@
 public class Counter {
     public String name;
-    public int counter;
+    public int count;
 
+    Counter(String name)
+    {
+        this(name,0);
+    }
+    Counter (String name, int count){
+        this.name = name;
+        this.count = count;
+    }
+    public void increment()
+    {
+        this.count++;
+    }
+    public void decrement()
+    {
+        this.count--;
+    }
+    public void info()
+    {
+        System.out.printf("%s: %d",name,count);
+        System.out.println();
+    }
 }
--- src/Program.java
+++ src/Program.java
@@ -1,7 +1,21 @@
 public class Program {
     static public  void  main (String[] args)
     {
+        Counter evenNumbers = new Counter("Четные числа");
+        Counter oddNumbers = new Counter("Нечетные числа");
 
+        for(int i = 1; i<= 100; i++)
+        {
+            if (i % 2 == 1)
+            {
+                evenNumbers.increment();
+            }
+            else {
+                oddNumbers.increment();
+            }
+        }
+        evenNumbers.info();
+        oddNumbers.info();
     }
 
 }
